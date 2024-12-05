ecd61e2edff0c6f8acf0974d7e6bba466aeaefbc
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-12-05 16:50:20 -0600

    create the intro component and begin adding aspects to it

---
+++ password_wizard_site/src/components/intro/intro.tsx
@@ -0,0 +1,13 @@
+
+function Intro() {
+    return (
+        <div className="intro">
+            <h1 className="intro-subtitle">Welcome to the Password Wizard!</h1>
+            <div className="intro-description">
+                <p>This application can generate a series of passwords that will help you increase your security throughout your life without having to come up with the passwords yourself.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Intro;
