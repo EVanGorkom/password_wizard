cdbcafd8cc7426fd5a2a4f3ebb1727eb871983c9
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-12-09 02:10:17 -0600

    Update the App.tsx and index.css files to account for the new components and styles

--- password_wizard_site/src/App.tsx
+++ password_wizard_site/src/App.tsx
@@ -2,29 +2,56 @@ import { useState } from 'react';
 import wizardLogo from './assets/password_wizard_logo_2.png';
 import './App.css';
 import { Intro } from './components/Intro/intro.tsx'
-import { PasswordOutput } from './components/PasswordOutput/passwordOutput';
+import { PasswordOptions } from './components/PasswordOption/passwordOption.tsx';
+import { SimplePassword } from './components/SimplePassword/simplePassword.tsx';
+import { CipherPassword } from './components/CipherPassword/cipherPassword.tsx';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [passwordType, setPasswordType] = useState<"simple" | "cipher">(
+    "simple"
+  );
+  const [cipherInput, setCipherInput] = useState("");
+
+  const handleCipherInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setCipherInput(event.target.value);
+  };
 
   return (
-    <>
+    <div className="app">
       <div>
         <img src={wizardLogo} className="logo" alt="Password Wizard logo" />
       </div>
+
       <h1>Password Wizard</h1>
+
       <div className="intro">
         <Intro />
       </div>
+
+      <div className="password-options">
+        <PasswordOptions setPasswordType={setPasswordType} />
+      </div>
+
       <div className="password-output">
-        <PasswordOutput />
+        {passwordType === "simple" ? (
+          <SimplePassword />
+        ) : (
+          <div>
+            <input
+              type="text"
+              placeholder="Enter text to cipher"
+              value={cipherInput}
+              onChange={handleCipherInputChange}
+              className="cipher-input"
+            />
+            <CipherPassword userInput={cipherInput} />
+          </div>
+        )}
       </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
+    </div>
   );
 }
 
