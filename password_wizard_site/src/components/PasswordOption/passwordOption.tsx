37c605c8ff086b7b35880f29d46d3ceb48d4971e
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-12-09 21:48:31 -0600

    Update the PoasswordOptions component for the new password options

--- password_wizard_site/src/components/PasswordOption/passwordOption.tsx
+++ password_wizard_site/src/components/PasswordOption/passwordOption.tsx
@@ -1,26 +1,41 @@
 interface PasswordOptionsProps {
-    setPasswordType: React.Dispatch<React.SetStateAction<"simple" | "cipher">>;
+    setPasswordType: React.Dispatch<
+        React.SetStateAction<"simple" | "balanced" | "strong" | "cipher">
+    >;
 }
 
 function PasswordOptions({ setPasswordType }: PasswordOptionsProps) {
-    const handleOptionChange = (type: "simple" | "cipher") => {
+    const handleOptionChange = (
+        type: "simple" | "balanced" | "strong" | "cipher") => {
         setPasswordType(type);
     };
 
     return (
         <div className="password-options">
-        <button
-            onClick={() => handleOptionChange("simple")}
-            className="option-button"
-        >
-            Create Simple Password
-        </button>
-        <button
-            onClick={() => handleOptionChange("cipher")}
-            className="option-button"
-        >
-            Jumble My Password
-        </button>
+            <button
+                onClick={() => handleOptionChange("simple")}
+                className="option-button"
+            >
+                Create Simple Password
+            </button>
+            <button
+                onClick={() => handleOptionChange("balanced")}
+                className="option-button"
+            >
+                Create Balanced Password
+            </button>
+            <button
+                onClick={() => handleOptionChange("strong")}
+                className="option-button"
+            >
+                Create Strong Password
+            </button>
+            <button
+                onClick={() => handleOptionChange("cipher")}
+                className="option-button"
+            >
+                Jumble My Password
+            </button>
         </div>
     );
 }
